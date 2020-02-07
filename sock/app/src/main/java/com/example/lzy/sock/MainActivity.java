abfc4a349bd91cf4633c03e397d95f795642979b
lzy <1690490917@qq.com>
2020-02-07 19:57:19 +0800

    锁屏界面

---
+++ sock/app/src/main/java/com/example/lzy/sock/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.lzy.sock;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.WindowManager;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED|WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON);
+    }
+}
